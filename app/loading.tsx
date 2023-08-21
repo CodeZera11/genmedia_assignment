f9712fb81ff180337cdecfeec358d6c3e092e51d
Bhavesh Yadav <codezera3@gmail.com>
2023-08-21 13:15:58 +0530

    completed basic project and installed shadcn ui

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='text-3xl text-white flex items-center justify-center w-full h-[100vh]'>Loading...</div>
+    )
+}
+
+export default Loading
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,145 @@
-import Image from 'next/image'
+"use client"
+
+import axios from 'axios';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  brand: string;
+  price: number;
+  images: string[];
+  category: string;
+}
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedPriceRange, setSelectedPriceRange] = useState<string>('');
+  const [sortBy, setSortBy] = useState<string>('name'); // default sort by name
+
+  useEffect(() => {
+
+    const getData = async () => {
+      try {
+        const response = await axios.get("https://dummyjson.com/products")
+
+        setProducts(response.data.products)
+
+        setFilteredProducts(response.data.products)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getData();
+
+  }, []);
+
+  const filterProducts = () => {
+    let filtered: Product[] = products
+
+    if (selectedCategory) {
+      filtered = filtered.filter((product) => product.category === selectedCategory);
+    }
+
+    if (selectedPriceRange) {
+      const [min, max] = selectedPriceRange.split('-');
+      filtered = filtered.filter((product) => product.price >= +min && product.price <= +max);
+    }
+
+    setFilteredProducts(filtered);
+  };
+
+  useEffect(() => {
+    filterProducts();
+  }, [selectedCategory, selectedPriceRange]);
+
+  const sortProducts = (value: string) => {
+    setSortBy(value);
+
+    const sorted = [...filteredProducts].sort((a, b) => {
+      if (value === 'price') {
+        return a.price - b.price;
+      }
+      // sort by name
+      return a.title.localeCompare(b.title);
+    });
+
+    setFilteredProducts(sorted);
+
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-col gap-20 py-10 bg-black w-full h-[100vh] text-white overflow-y-scroll'>
+      <div className='flex flex-col gap-10 items-center justify-center'>
+        <h1 className='text-4xl font-bold'>Products List</h1>
+        <div className='flex gap-10'>
+          <div >
+            <label htmlFor="category">Filter by Category: </label>
+            <select className='rounded-full text-black p-2 bg-gray-400 ml-2'
+              id="category"
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+              <option className='text-center' value="">All</option>
+              <option className='text-center' value="smartphones">Smartphones</option>
+              <option className='text-center' value="laptops">Laptops</option>
+              <option className='text-center' value="fragrances">Fragrances</option>
+              <option className='text-center' value="groceries">Groceries</option>
+              <option className='text-center' value="furniture">Furniture</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="priceRange">Filter by Price Range: </label>
+            <select className='rounded-full text-black  p-2 bg-gray-400 ml-2'
+              id="priceRange"
+              value={selectedPriceRange}
+              onChange={(e) => setSelectedPriceRange(e.target.value)}
+            >
+              <option className='text-center ' value="">All</option>
+              <option className='text-center' value="0-10">0-10</option>
+              <option className='text-center' value="10-50">10-50</option>
+              <option className='text-center' value="50-100">50-100</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="sortBy">Sort by: </label>
+            <select className='rounded-full text-black p-2 bg-gray-400 ml-2'
+              id="sortBy"
+              value={sortBy}
+              onChange={(e) => sortProducts(e.target.value)}
+            >
+              <option className='text-center' value="name">Name</option>
+              <option className='text-center' value="price">Price</option>
+            </select>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='grid grid-cols-4 gap-5'>
+        {(filteredProducts.map((product, index) => (
+          <div key={index} className='flex flex-col border border-gray-500'>
+            <Image
+              src={product.images[0]}
+              alt={product.title}
+              width={1000}
+              height={1000}
+              className='w-full object-fill h-[300px] inset-0'
+            />
+            <div className='flex flex-col gap-1 px-4 py-2'>
+              <h1 className='font-mono text-lg '>{product.title}</h1>
+              <p className='text-gray-400'>₹{product.price}/-</p>
+            </div>
+          </div>
+        )))}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
